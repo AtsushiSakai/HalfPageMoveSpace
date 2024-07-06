0fb4c5bb8f391f64e9b290f05c2526d95959b198
Atsushi Sakai <asakai.amsl+github@gmail.com>
2024-07-06 19:18:39 +0900

    release 0.3

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
 document.addEventListener('keydown', function(event) {
+
+  // Skip when text area is focused.
+  if (event.target.tagName.toLowerCase() === 'textarea') {
+    return;
+  }
+
   if (event.key === ' ') {
     event.preventDefault();
     window.scrollBy(0, window.innerHeight / 2);
